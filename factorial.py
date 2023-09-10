998d7b8facf28961d0dcdac1f3378d72609a8899
ArunaKandarajan <144599389+ArunaKandarajan@users.noreply.github.com>
2023-09-10 20:03:40 +0530

    Create factorial of a given number
    To find the factorial of a given number

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def recurfact(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * (recurfact(n - 1))
+
+
+number = int(input("ENTER A NUMBER:"))
+res = recurfact(number)
+print(
+  "THE FACTORIAL OF",
+  number,
+  "IS",
+  res,
+)
